120f7087dccd901bcee87a92c7b2f280640fb754
Sabinkari Rahul <sabinkari.rahul@expleogroup.com>
2023-01-25 20:17:46 +0530

    initial commit

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Congratulations, it's a web app!"
+    return "Congratulations, INDIA"
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
